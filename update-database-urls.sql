0b62f8ce87420bcc5d778bbbe40933d04c846a3b
Arun Sahadeo <arunjamessahadeo@gmail.com>
2019-05-04 23:42:39 +0100

    Quote values

--- update-database-urls.sql
+++ update-database-urls.sql
@@ -8,10 +8,10 @@ SET @live_domain = 'https://example.com';
 SET @local_domain = 'http://example.com';
 
 SET @stmt_one = CONCAT('USE ', @database_name, '\n');
-SET @stmt_two = CONCAT('UPDATE ', @table_prefix, 'options SET option_value = REPLACE(option_value, ', @live_domain, ', ', @local_domain, ');\n');
-SET @stmt_three = CONCAT('UPDATE ', @table_prefix, 'posts SET guid = REPLACE(guid, ', @live_domain, ', ', @local_domain, ');\n');
-SET @stmt_four = CONCAT('UPDATE ', @table_prefix, 'posts SET post_content = REPLACE(post_content, ', @live_domain, ', ', @local_domain, ');\n');
-SET @stmt_five = CONCAT('UPDATE ', @table_prefix, 'postmeta SET meta_value = REPLACE(meta_value, ', @live_domain, ', ', @local_domain, ');\n');
+SET @stmt_two = CONCAT('UPDATE ', @table_prefix, 'options SET option_value = REPLACE(option_value, "', @live_domain, '", "', @local_domain, '");\n');
+SET @stmt_three = CONCAT('UPDATE ', @table_prefix, 'posts SET guid = REPLACE(guid, "', @live_domain, '", "', @local_domain, '");\n');
+SET @stmt_four = CONCAT('UPDATE ', @table_prefix, 'posts SET post_content = REPLACE(post_content, "', @live_domain, '", "', @local_domain, '");\n');
+SET @stmt_five = CONCAT('UPDATE ', @table_prefix, 'postmeta SET meta_value = REPLACE(meta_value, "', @live_domain, '", "', @local_domain, '");\n');
 
 SET @sql = CONCAT(@stmt_one, @stmt_two, @stmt_three, @stmt_four, @stmt_five);
 

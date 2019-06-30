41dd4db3178f6093bd1be1de4ceb43c1e89a663a
Arun Sahadeo <arunjamessahadeo@gmail.com>
2019-06-30 11:26:03 +0100

    Update script to split prepared statement into multiple prepared statements

--- update-database-urls.sql
+++ update-database-urls.sql
@@ -3,17 +3,27 @@
 */
 
 SET @table_prefix = 'wp_';
-SET @database_name = 'sample_database';
-SET @live_domain = 'https://example.com';
+SET @database = 'wordpress';
+SET @live_domain = 'http://test.wp';
 SET @local_domain = 'http://example.com';
 
-SET @stmt_one = CONCAT('USE ', @database_name, '\n');
-SET @stmt_two = CONCAT('UPDATE ', @table_prefix, 'options SET option_value = REPLACE(option_value, "', @live_domain, '", "', @local_domain, '");\n');
-SET @stmt_three = CONCAT('UPDATE ', @table_prefix, 'posts SET guid = REPLACE(guid, "', @live_domain, '", "', @local_domain, '");\n');
-SET @stmt_four = CONCAT('UPDATE ', @table_prefix, 'posts SET post_content = REPLACE(post_content, "', @live_domain, '", "', @local_domain, '");\n');
-SET @stmt_five = CONCAT('UPDATE ', @table_prefix, 'postmeta SET meta_value = REPLACE(meta_value, "', @live_domain, '", "', @local_domain, '");\n');
+SET @stmt_one = CONCAT('UPDATE ', @database, '.', @table_prefix, 'options SET option_value = REPLACE(option_value, "', @live_domain, '", "', @local_domain, '");\n');
+SET @stmt_two = CONCAT('UPDATE ', @database, '.', @table_prefix, 'posts SET guid = REPLACE(guid, "', @live_domain, '", "', @local_domain, '");\n');
+SET @stmt_three = CONCAT('UPDATE ', @database, '.', @table_prefix, 'posts SET post_content = REPLACE(post_content, "', @live_domain, '", "', @local_domain, '");\n');
+SET @stmt_four = CONCAT('UPDATE ', @database, '.', @table_prefix, 'postmeta SET meta_value = REPLACE(meta_value, "', @live_domain, '", "', @local_domain, '");\n');
 
-SET @sql = CONCAT(@stmt_one, @stmt_two, @stmt_three, @stmt_four, @stmt_five);
-
-PREPARE stmt from @sql;
+PREPARE stmt from @stmt_one;
 EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+PREPARE stmt from @stmt_two;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+PREPARE stmt from @stmt_three;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+PREPARE stmt from @stmt_four;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;

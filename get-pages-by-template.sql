6782923f68896e5e028505cec4e1db44d34cc78c
Arun Sahadeo <arunjamessahadeo@gmail.com>
2019-06-30 11:29:49 +0100

    Update script to include database name

--- get-pages-by-template.sql
+++ get-pages-by-template.sql
@@ -3,9 +3,10 @@
 */
 
 SET @table_prefix = 'wp_';
+SET @database = 'wordpress';
 SET @template_name = 'template-name.php';
-SET @stmt_one = CONCAT('SELECT * FROM ', @table_prefix, 'posts as posts\n');
-SET @stmt_two = CONCAT('INNER JOIN ', @table_prefix, 'postmeta pm ON posts.id = pm.post_id\n');
+SET @stmt_one = CONCAT('SELECT * FROM ', @database, '.', @table_prefix, 'posts as posts\n');
+SET @stmt_two = CONCAT('INNER JOIN ', @database, '.', @table_prefix, 'postmeta pm ON posts.id = pm.post_id\n');
 SET @stmt_three = CONCAT('WHERE pm.meta_key = "_wp_page_template" AND pm.meta_value ="', @template_name, '"\n');
 SET @stmt_four = CONCAT('ORDER BY posts.post_name ASC');
 SET @sql = CONCAT(@stmt_one, @stmt_two, @stmt_three, @stmt_four);

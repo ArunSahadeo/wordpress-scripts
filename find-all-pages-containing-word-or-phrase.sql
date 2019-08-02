cdc7abc79b83ede82e07b865537af4cdf70598b7
Arun Sahadeo <arunjamessahadeo@gmail.com>
2019-08-02 21:31:57 +0100

    Include post type

--- find-all-pages-containing-word-or-phrase.sql
+++ find-all-pages-containing-word-or-phrase.sql
@@ -6,7 +6,7 @@ SET @table_prefix = 'wp_';
 SET @database = 'database_name';
 SET @expression = 'expression to search';
 
-SET @stmt_one = CONCAT('SELECT DISTINCT post.id, post.post_title FROM ', @database, '.', @table_prefix, 'posts AS post WHERE post.post_content LIKE "', @expression, '"\n');
+SET @stmt_one = CONCAT('SELECT DISTINCT post.id, post.post_title, post.post_type FROM ', @database, '.', @table_prefix, 'posts AS post WHERE post.post_content LIKE "', @expression, '"\n');
 SET @stmt_two = CONCAT('ORDER BY post.id DESC');
 
 SET @sql = CONCAT(@stmt_one, @stmt_two);
@@ -15,7 +15,7 @@ PREPARE stmt from @sql;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
-SET @stmt_one = CONCAT('SELECT DISTINCT post.id, post.post_title FROM ', @database, '.', @table_prefix, 'postmeta AS pm\n');
+SET @stmt_one = CONCAT('SELECT DISTINCT post.id, post.post_title, post.post_type FROM ', @database, '.', @table_prefix, 'postmeta AS pm\n');
 SET @stmt_two = CONCAT('INNER JOIN ', @database, '.', @table_prefix, 'posts post ON post.id = pm.post_id\n');
 SET @stmt_three = CONCAT('WHERE pm.meta_value LIKE "%', @expression, '%"\n');
 SET @stmt_four = CONCAT('ORDER BY post.id DESC');
